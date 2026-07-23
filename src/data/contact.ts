4fe830c47eacbd4d412899c6e819c82a1377be76
Martin-Juncos <prof.mcjuncos@gmail.com>
2026-07-23 10:16:39 -0300

    cambio de numero de telefono

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -1,11 +1,11 @@
 export const contactInfo = {
   domain: "modobeta.net.ar",
-  whatsappNumber: "+54 9 3777 679100",
-  whatsappBaseUrl: "https://wa.me/5493777679100",
+  whatsappNumber: "+54 9 3777 738080",
+  whatsappBaseUrl: "https://wa.me/5493777738080",
   whatsappMessage:
     "Hola ModoBeta, quiero consultar por una solución digital para mi institución/proyecto.",
   whatsappLink:
-    "https://wa.me/5493777679100?text=Hola%20ModoBeta%2C%20quiero%20consultar%20por%20una%20soluci%C3%B3n%20digital%20para%20mi%20instituci%C3%B3n%2Fproyecto.",
+    "https://wa.me/5493777738080?text=Hola%20ModoBeta%2C%20quiero%20consultar%20por%20una%20soluci%C3%B3n%20digital%20para%20mi%20instituci%C3%B3n%2Fproyecto.",
   email: "modo.beta.developer@gmail.com",
   instagram: "https://www.instagram.com/modobeta.ok/",
   instagramLabel: "@modobeta.ok",
